8a7050c32150515cc0db02ca40fa1c285e9556ed
EthyMoney <ultimate360solutions@gmail.com>
2024-12-20 10:57:56 -0600

    remove rpm and macos make targets

--- forge.config.js
+++ forge.config.js
@@ -12,17 +12,9 @@ module.exports = {
       config: {},
     },
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
       name: '@electron-forge/maker-deb',
       config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
+    }
   ],
   plugins: [
     {
