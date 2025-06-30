0d58e0b86600e49bef64448c5805768edd4a9961
Logan <ultimate360solutions@gmail.com>
2025-06-29 19:54:33 -0500

    made modifications needed for 1024x600 on 32-bit pi

--- src/main.js
+++ src/main.js
@@ -39,8 +39,8 @@ const createWindow = () => {
 
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 480,
-    height: 320,
+    width: 1024,
+    height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
